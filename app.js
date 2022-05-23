dc5dc550e1d1b9e01f4a674a5bc07b4accd212ca
qaiswardag <qais.wardag@outlook.com>
2022-05-23 21:26:56 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'join_us'
+});
+
+const query = 'SELECT * FROM test';
+connection.query(query, (error, results, fields) => {
+    if (error) throw error;
+
+    try {
+        console.log('results: ', results);
+    } catch (err) {
+        console.log('there have been an error. Error is:', err)
+    }
+});
+
+
+connection.end();
---
+++ script.js
@@ -0,0 +1,4 @@
+// import {myArray} from "./faker.js";
+// console.log('my script file. imported array is:', myArray)
+
+
