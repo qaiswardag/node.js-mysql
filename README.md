e271d22049d597df87d06ec4e5724d3fddd7cadd
qaiswardag <qais.wardag@outlook.com>
2022-05-23 22:56:43 +0200

    Add shema.sql

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Node.js and MySQL
+
+A pure node.js JavaScript Client implementing the MySQL protocol.
+<br>
+Documentation: [mysqljs/ mysql](https://github.com/mysqljs/mysql)
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const {faker} = require('@faker-js/faker');
 const mysql = require('mysql');
 
-// start connection
+// init connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -8,17 +9,68 @@ const connection = mysql.createConnection({
     database: 'join_us'
 });
 
-const query = 'SELECT * FROM test';
-connection.query(query, (error, results, fields) => {
-    if (error) throw error;
+// // query
+// const queryUsers = 'SELECT * FROM users';
+// // run sql code
+// connection.query(queryUsers, (error, results, fields) => {
+//     if (error) throw error;
+//     console.log('Error:', error)
+//     console.log('results users:', results);
+// });
 
-    try {
-        console.log('results: ', results);
-    } catch (err) {
-        console.log('there have been an error. Error is:', err)
-    }
+// // query count
+// const queryCount = 'SELECT COUNT(*) FROM users';
+// // run sql code
+// connection.query(queryCount, (error, results, fields) => {
+//     if (error) throw error;
+//     console.log('Error:', error)
+//     console.log('results count:', results);
+// });
+
+
+// // insert data
+// const person = {
+//     email: faker.internet.email(),
+//     created_at: faker.date.past()
+// };
+//
+// var end_result = connection.query('INSERT INTO users SET ?', person, function (err, result) {
+//     if (err) throw err;
+//     console.log(result);
+// });
+//
+//
+// // query
+// const queryUsers = 'SELECT * FROM users';
+// // run sql code
+// connection.query(queryUsers, (error, results, fields) => {
+//     if (error) throw error;
+//     console.log('Error:', error)
+//     console.log('results users:', results);
+// });
+
+
+//
+//
+//
+//
+//
+// insert 500 persons
+const data = [];
+for (var i = 0; i < 500; i++) {
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+
+
+var query = 'INSERT INTO users (email, created_at) VALUES ?';
+
+connection.query(query, [data], function (err, result) {
+    console.log(err);
+    console.log('result:', result);
 });
-
 
 // end connection
 connection.end();
